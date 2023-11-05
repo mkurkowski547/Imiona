e2ea6a46c95fc3e9932a43252788eb6a05dcd5a8
szefik <szefik@DESKTOP-AIAOQFC>
2023-11-05 13:29:46 +0100

    Dodaj pliki projektów.

---
+++ Imiona/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Imiona
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listaImion = new List<string>
+        {
+            "Justyna",
+            "Piotr",
+            "Mateusz",
+            "Aleksandra",
+            "Bartłomiej",
+            "Stanisław",
+            "Dorota",
+            "Michał"
+        };
+            foreach (string imie in listaImion)
+            {
+                Powiedz(imie);
+            }
+        }
+        public static void Powiedz(string imie)
+        {
+            Console.WriteLine("Jedno dla {0}, jedno dla mnie.", imie);
+
+        }
+    }
+}
